2d50b23c782130ba98e276620d7aa74250f315cb
gem-shubhangini <110813757+gem-shubhangini@users.noreply.github.com>
2022-10-10 14:06:22 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 const routes: Routes = [
   {
-    path: '', redirectTo:'user/create',pathMatch:"full"
+    path: 'UserModule', redirectTo:'UserModule/user/create',pathMatch:"full"
    
   },
   {
-    path: 'user',
+    path: 'UserModule/user',
     children: [
       {
         path: 'create',
