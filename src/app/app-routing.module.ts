b6f3a8d8fa4a8a054428801ba2fd4a3e6b82f929
SHUBHANGINI SHARMA <SHUBHANGINISHARMA67@gmail.com>
2022-10-10 13:32:36 +0530

    changes in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 const routes: Routes = [
   {
-    path: '', redirectTo:'user',pathMatch:"full"
+    path: '', redirectTo:'user/create',pathMatch:"full"
    
   },
   {
-    path: 'user',redirectTo:'user/create',pathMatch:"full"
+    path: 'user',
     children: [
       {
         path: 'create',
